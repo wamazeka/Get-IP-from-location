2216fcda659930b1899790a46392d813feb687d3
wamazeka <7118706+wamazeka@users.noreply.github.com>
2023-03-18 21:38:48 +0400

    Init commit, main functionality

---
+++ ip_from_geo.py
@@ -0,0 +1,54 @@
+import csv
+import random
+import ipaddress
+
+class IPFaker:
+
+    def __init__(self,
+                 blocksFile="GeoLite2-Country-Blocks-IPv4.csv",
+                 locationFile="GeoLite2-Country-Locations-en.csv") :
+
+        self.countriesIPblocks = {}
+        self.id2code = {}
+
+        with open("GeoLite2-Country-Locations-en.csv", "r") as f:
+            reader = csv.DictReader(f)
+            next(reader)
+            for row in reader:
+                self.id2code[row["geoname_id"]] = row["country_iso_code"]
+                self.countriesIPblocks[row["country_iso_code"]] = []
+
+        with open("GeoLite2-Country-Blocks-IPv4.csv", "r") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                try:
+                    self.countriesIPblocks[self.id2code[row['geoname_id']]].append(row["network"])
+                except:
+                    pass
+
+
+    def getip(self, country="ANY"):
+        if country == "ANY":
+            country = random.choice(list(self.countriesIPblocks.keys()))
+
+        mask = ipaddress.IPv4Network(random.choice(self.countriesIPblocks[country]))
+        ip = random.choice(list(mask.hosts()))
+
+        return str(ip)
+
+    def getiplist(self, country="ANY", count=10):
+        list = []
+        for i in range(count):
+            list.append(str(self.getip(country)))
+        return list
+
+
+
+if __name__ == '__main__':
+    ipgetter = IPFaker()
+
+    print(ipgetter.getip())
+    print(ipgetter.getip(country='US'))
+    print(ipgetter.getiplist(country='IT', count=14))
+
+
